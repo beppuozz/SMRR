d01bf072eb6a2c038606b3dc8b0d83df46e07b99
ichardforphys <r.mifredi@campus.unimib.it>
2026-01-01 23:15:08 +0100

    Main of LaBr3 simulation is done.

--- GEANT4/LaBr3/LaBr3.cc
+++ GEANT4/LaBr3/LaBr3.cc
@@ -1,6 +1,6 @@
 /*This is the main file for the LaBr3 simulation project*/
 
-//-------------------------------------------- INCLUDES ----------------------------------------------------
+/*================================================Includes================================================*/
 #include "G4RunManager.hh" // run manager
 #include "G4UIManger.hh" //manager user interface
 #include "G4Version.hh" //compatibilità tra versioni di geat4
@@ -15,7 +15,75 @@
 
 #include "DetectorConstruction.hh" // includo lib per geometry
 #include "PrimayGEneratorAction.hh" // includo lib per interazione
-#include "QGSP_BERT_HP.hh"  // QUAK GLUO SYMMETRY...
+#include "QGSP_BERT_HP.hh"  // QUARK GLUO SYMMETRY...
 
 #include "EventAction.hh"
 #include "RunAction.hh"
+
+/*================================================MAIN================================================*/
+
+int main (int argc, char* argv){
+    //Choose the andom GEnerator Engine
+    //This is the same as TRandom3 ROOT generator, it has a period of 2^19937-1
+    CLEP::HepRandom::setTheEngine(new CLEP::MTwistEngine()) //   
+
+    G4RunManager* runManager = new G4RunManager();   
+
+    /*====================Mandatory Initialization Classes====================*/
+    G4VUserDetectionConstruction* detector = new DetectorConstruction();
+    //V sta per virtual...uno chassis di una classe che Geant mette a disposizione, i cui metodi sono defiti ma non implementati
+    //Dunque dovremo crere una classe chiamata DetectorConstruction all'interno della quale definiamo tutto quello che riguarda il nostro detector
+    runManager->SetUserInitialization(detector);
+
+    G4cout << "Setting Physics List" << G4endl;
+    G4UserPhysicsList* physics = new QGSP_BERT_HP(); //stesso discorso per DetectorConstruction
+    runManager->SetUserInitialization(physics);
+
+    /*====================Mandatory Action Classes====================*/
+    G4VUserPrimaryGeneratorAction* gen_action = new PrimaryGeneratorAction();
+    runManager->SetUserAction(get_action);
+
+    /*====================Optional USer Action Classes====================*/
+    EventAction* event_action = new EventAction();
+    RunAction* run_action = new RunAction(event_action);
+    runManager->SetUserAction(event_action);
+    runManager->SetUserAction(run_action);
+
+    /*====================Initialize the G4 Kernel====================*/
+    runManager->Initialize();
+
+    /*====================Visualization and UI====================*/
+
+    G4VisManager* visManager = new G4VisExecutive();
+    visManager->Initialize();
+
+    G4UImanager* UImanager = G4UIManager::GetUIpointer();
+
+    if(argc!=1)    {
+        //Batch mode --> faccio partire la simulazione senza visualizzazione nè altro...solo risultati numerici
+        G4String command = "/control/execute";
+        G4String filename = argv[1];
+        UIManager->ApplyCommand(command+filename);        
+    }
+    else {
+        #if G4VERSION_NUMBER>=930
+            //Interactive Mode: define UI session
+            G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+            if (ui->isGUI()) UImanager->ApplyCommand("control/execute/visQT.mac"); // se la UI è una GuidedUI, devo utilizzare il programma qt
+            else UImanager->ApplyCommand("/control/execute vis.mac");
+        #else
+            #ifdef G4UI_USE_TCSH //è una variabile d'ambiente. Se è definita, usa shell tcsh per eseguire comando, altrimenti usa default
+                G4UIsession* ui = new G4UITerminal(new G4UItcsh);
+            #else 
+                G4UIsession* ui = new G4UITerminal();
+            #endif 
+                UImanager->ApplyCommand("/control/execute vis.mac")
+        #endif
+            ui->SessionStart();
+            delete ui;
+    }
+
+    delete runManager;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -28,7 +28,7 @@ $ cmake --build . -- -j$(nproc)
 $ cmake --install .
 ```
 ---
-### Folder Structure
+### Folder Structure 
 TaskNmr
 * TaskNmr --> prima sottocartella, contiene i file principali
     * include --> contains all .hh files
@@ -44,9 +44,10 @@ Follow this --> https://geant4.web.cern.ch/documentation/pipelines/master/bfad_h
 ```bash
 $ mkdir example_build
 $ cd example_build
-$ cmake -DCMAKE_PREFIX_PATH=/home/you/geant4-install /path/to/example
+$ cmake -DCMAKE_PREFIX_PATH=/home/ricca/SMRR/GEANT4/geant4_install  /home/ricca/SMRR/GEANT4/geant4/examples/basic/B1
 $ make -j4 VERBOSE=1
 ```
+
 Nel caso in cui in fase di compilazione dovesse apparire il fatal error "ENSDATA.dat not found", 
 Comandi per risolvere ENSDATA.dat not found:
 
@@ -57,6 +58,12 @@ $ export G4RADIOACTIVEDATA=/home/ubuntu/SMRR/GEANT4/geant4_install/share/Geant4/
 $ export G4NEUTRONHPDATA=/home/ubuntu/SMRR/GEANT4/geant4_install/share/Geant4/data/G4NDL4.7.1
 $ export G4SAIDXSDATA=/home/ubuntu/SMRR/GEANT4/geant4_install/share/Geant4/data/G4SAIDDATA2.0
 $ export G4LEDATA=/home/ubuntu/SMRR/GEANT4/geant4_install/share/Geant4/data/G4EMLOW8.8
+```
+
+### How to execute
+```bash
+$ source /home/ricca/SMRR/GEANT4/geant4_install/bin/geant4.sh
+$ ./example
 ```
 ---
 
